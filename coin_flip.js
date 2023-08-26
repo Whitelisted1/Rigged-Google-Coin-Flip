892e0b517d7ecfd508fdaec2fb38e936f2b49e22
Whitelisted <77711834+Whitelisted1@users.noreply.github.com>
2023-08-26 07:54:14 -0600

    Add options page
    Added an options page that allows you to change the outcome of the Coin Flip Easily

--- coin_flip.js
+++ coin_flip.js
@@ -1,7 +1,28 @@
-console.log("Loaded background google coin flip thing");
-
 // Wait for a specific amount of ms
 function delay(ms) { return new Promise(resolve => setTimeout(resolve, ms)); }
+
+async function getData(key){ return new Promise(resolve => { chrome.storage.local.get(key, function (data) { resolve(Object.values(data)[0]); }); }); }
+async function storeData(key, value){ await chrome.storage.local.set({[key]: value}); }
+
+async function getMultipleDataValues(keys){ return new Promise(resolve => { chrome.storage.local.get(keys, function (data) { resolve(data); }); }) }
+async function storeMultipleDataValues(keys){ await chrome.storage.local.set(keys); }
+
+
+// Get local settings
+async function getSettings() {
+    settings = await getMultipleDataValues(settingsKeys);
+
+    for (var i = 0; i < settingsKeys.length; i++) {
+        thisKey = settingsKeys[i];
+        thisValue = settings[thisKey];
+        if (thisValue == undefined) {
+            settings[thisKey] = settingsDefaultValues[i];
+            await storeData(thisKey, thisValue);
+        }
+    }
+
+    return settings;
+}
 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
@@ -9,8 +30,19 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
+
+const settingsKeys = [
+    'extensionEnabled',
+    'targetHeads'
+];
+const settingsDefaultValues = [
+    true,
+    true
+];
+
 let fakeCoin;
 let newStartButton;
+let settings;
 let flipping = false;
 
 async function flipCoin() {
@@ -23,7 +55,12 @@ async function flipCoin() {
     const headsIndex = 0;
     const tailsIndex = 9;
     
-    targetIndex = tailsIndex;
+    // To update if it changes in the action menu
+    if (await getData("targetHeads") == true) {
+        targetIndex = headsIndex
+    } else {
+        targetIndex = tailsIndex;
+    }
     
     const numRotations = getRandomInt(1, 3);
     // const timeForCoinToRotate = getRandomInt(1250, 1500); // 1250
@@ -42,9 +79,6 @@ async function flipCoin() {
         coinFlipCanvas = document.querySelector('canvas[jsname="UzWXSb"]');
         coinFlipSheet = document.querySelector('img[jsname="gtVczb"]');
         startButtonParent = document.querySelector('button[jsname="prQWAb"]').parentElement;
-        
-
-        console.log(coinFlipSheet);
         
         if (coinFlipCanvas == undefined) {
             console.log("Can't find coinflip");
@@ -72,33 +106,19 @@ async function flipCoin() {
 
         startButtonParent.appendChild(newStartButton);
         
-        // childList = coinFlipCanvas.parentElement.parentElement.childNodes;
-        // coinFlipSheet = childList[childList.length-1].firstChild.src;
-        // console.log(coinFlipSheet);
-        // console.log(coinFlipCanvas);
-        
         wrapper = document.createElement("div");
         wrapper.classList.add("CoinFlipWrapper");
-        
+
         fakeCoin = document.createElement("img");
         fakeCoin.classList.add("CoinToFlip");
         fakeCoin.src = "/logos/fnbx/coin_flip/coin_sheet.png";
         fakeCoin.width = "150";
         fakeCoin.style.width = "150px";
         
-        // t = 0;
-        // while (coinFlipCanvas.width != 150) {
-        //     t++;
-        //     if (t > 20) return;
-        
-        //     await delay(100);
-        // }
-        
-        wrapper.appendChild(fakeCoin)
+        wrapper.appendChild(fakeCoin);
         coinFlipCanvas.parentElement.appendChild(wrapper);
         
         coinFlipCanvas.remove();
-        // coinFlipCanvas.style.display = "none";
     }
         
     newStartButton.innerText = "Flipping...";
@@ -116,7 +136,6 @@ async function flipCoin() {
         fakeCoin.style.animationDirection = "normal";
     }
     
-    
     fakeCoin.classList.add("CoinFlipping");
 
     targetUnix = Date.now() + (numRotations * timeForCoinToRotate);
@@ -128,7 +147,7 @@ async function flipCoin() {
     while (true) {
         timesToRotate--;
         if (timesToRotate == 0) break;
-        await delay( ((targetUnix - Date.now()) / timesToRotate) - 0 );
+
         // i++;
         // i = (i % numSides)+1
         i = ( (i+1) % (numSides) )
@@ -136,9 +155,7 @@ async function flipCoin() {
             numFlips++;
         }
         
-        // console.log(i);
-        
-        fakeCoin.style.transformOrigin = "50%" + ((150 * i) + 75) + "px";
+        fakeCoin.style.transformOrigin = "50% " + ((150 * i) + 75) + "px";
         // fakeCoin.style.transformOrigin = (150 * i) + 75 + "px 50%";
         fakeCoin.style.top = "-" + 150 * i + "px";
         
@@ -147,10 +164,11 @@ async function flipCoin() {
         
         // fakeCoin.style.transform = "translateY(-" + 150 * i + "px)"
         // if (i == targetIndex && animationEnded) {
-            //     console.log(i, numFlips)
-            //     // clearInterval(interval);
-            //     break;
+        //     console.log(i, numFlips)
+        //     // clearInterval(interval);
+        //     break;
         // }
+        await delay( ((targetUnix - Date.now()) / timesToRotate) - 0 );
     }
 
     fakeCoin.classList.remove("CoinFlipping");
@@ -173,11 +191,14 @@ async function flipCoin() {
     flipping = false;
 }
 
-(async() => {
-    await flipCoin();
+// document.querySelector('canvas[jsname="UzWXSb"]').style.opacity = 0;
 
-    // startButton = document.querySelector('button[jsname="prQWAb"]');
-    // startButton.addEventListener("click", async () => {
-    //     await flipCoin();
-    // });
+(async() => {
+    settings = await getSettings();
+
+    if (!settings.extensionEnabled) {
+        return;
+    }
+
+    await flipCoin();
 })();
---
+++ options/options.js
@@ -0,0 +1,139 @@
+async function getData(key){ return Object.values(await chrome.storage.local.get(key))[0]; }
+async function storeData(key, value){ await chrome.storage.local.set({[key]: value}); }
+
+async function getMultipleDataValues(keys){ return await chrome.storage.local.get(keys); }
+async function storeMultipleDataValues(keys){ await chrome.storage.local.set(keys); }
+
+const STRING_TYPE = 0;
+const INTEGER_TYPE = 1;
+const BOOL_TYPE = 2;
+const ARRAY_TYPE = 3;
+
+const settingsKeys = [
+    'extensionEnabled',
+    'targetHeads'
+];
+
+const settingsDefaultValues = [
+    true,
+    true
+];
+
+const settingsKeyTypes = [
+    BOOL_TYPE,
+    BOOL_TYPE
+];
+
+async function shouldUpdate() {
+    onlineVersionData = await getData("onlineVersion");
+    if (onlineVersionData == undefined || (onlineVersionData.expiresAt - Date.now() < 0)) {
+        console.log("Fetching remote version");
+        let updateURL = "https://raw.githubusercontent.com/Whitelisted1/Google-Coin-Flip-Rigger/main/manifest.json";
+        
+        res = await fetch(updateURL);
+
+        if (res.status != 200) {
+            console.warn("Unable to fetch remote version");
+            return;
+        }
+
+        onlineManifest = await res.json();
+        onlineVersion = onlineManifest['version'];
+        
+        // Store in cache for a day
+        storeData('onlineVersion', {'version': onlineVersion, expiresAt: Date.now() + 60 * 60 * 24 * 1000});
+    } else {
+        console.log("Using cache for remote version");
+        onlineVersion = onlineVersionData['version'];
+    }
+    
+    let manifestURL = chrome.runtime.getURL('manifest.json');
+    
+    res = await fetch(manifestURL);
+    localManifest = await res.json();
+    localVersion = localManifest['version'];
+
+    if (localVersion != onlineVersion) {
+        console.log("An update is available (" + localVersion + " >> " + onlineVersion + ")");
+
+        updateElement = document.getElementById("updateText");
+        updateElement.innerText = "An update is available (" + localVersion + " >> " + onlineVersion + ")";
+    }
+}
+
+async function saveSettings() {
+    storedSettingsValues = { };
+
+    for (var i = 0; i < settingsKeys.length; i++) {
+        thisKey = settingsKeys[i];
+        thisType = settingsKeyTypes[i];
+
+        element = document.getElementById(thisKey);
+
+        if (thisType == STRING_TYPE || thisType == INTEGER_TYPE) {
+            storedSettingsValues[thisKey] = element.value;
+        }
+        else if (thisType == BOOL_TYPE) {
+            storedSettingsValues[thisKey] = element.checked;
+        }
+    }
+
+
+    await storeMultipleDataValues(storedSettingsValues);
+}
+
+async function fetchAndDisplaySettings() {
+    const settingsValues = await getMultipleDataValues(settingsKeys);
+
+    for (var i = 0; i < settingsKeys.length; i++) {
+        thisKey = settingsKeys[i];
+        thisType = settingsKeyTypes[i];
+        keyValue = settingsValues[thisKey];
+
+        if (keyValue == undefined) {
+            thisKeyDefaultValue = settingsDefaultValues[i];
+            await storeData(thisKey, thisKeyDefaultValue);
+            settingsValues[thisKey] = thisKeyDefaultValue;
+            i--;
+            continue;
+        }
+
+        element = document.getElementById(thisKey);
+
+        element.addEventListener("change", async (e) => {
+            thisType = settingsKeyTypes[ settingsKeys.indexOf(e.srcElement.id) ];
+            
+            if (thisType == STRING_TYPE || thisType == INTEGER_TYPE) {
+                await storeData(e.srcElement.id, e.srcElement.value);
+            }
+            else if (thisType == BOOL_TYPE) {
+                await storeData(e.srcElement.id, e.srcElement.checked);
+            }
+        });
+
+        if (thisType == STRING_TYPE || thisType == INTEGER_TYPE) {
+            element.value = keyValue;
+        }
+        else if (thisType == BOOL_TYPE) {
+            element.checked = keyValue;
+        }
+
+    }
+}
+
+window.addEventListener("load", async () => {
+    console.log(await getData("extensionEnabled"));
+    console.log(await getMultipleDataValues(["extensionEnabled", "targetHeads"]));
+
+    document.getElementById("resetSettings").addEventListener("click", () => {
+        chrome.storage.local.clear();
+        fetchAndDisplaySettings();
+    });
+
+    // document.getElementById("applySettings").addEventListener("click", () => {
+    //     saveSettings();
+    // });
+
+    await fetchAndDisplaySettings();
+    shouldUpdate();
+});
